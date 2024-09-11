1b9b800437aa3276a2d1ce0aef6bd10499af1d85
NS NooR <non377938@gmail.com>
2024-09-12 03:20:37 +0600

    Hotel_Booking

---
+++ lib/view/screen/welcome/widget/where_field.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:hotel_booking_app/utils/app_color.dart';
+
+class WhereField extends StatelessWidget {
+  const WhereField({
+    super.key, required this.whereController,
+  });
+  final TextEditingController whereController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: whereController,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(
+          Icons.location_on_outlined,
+          color: Colors.grey,
+        ),
+        enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: Colors.grey),
+            borderRadius: BorderRadius.circular(10)),
+        focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(color: AppColor.primaryColor),
+            borderRadius: BorderRadius.circular(10)),
+      ),
+    );
+  }
+}
