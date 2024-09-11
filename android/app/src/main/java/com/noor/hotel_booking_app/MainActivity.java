08c01d749d418c8c92dd723e06a25b1b6a376ceb
NS NooR <non377938@gmail.com>
2024-09-11 15:22:12 +0600

    Hotel_Booking

---
+++ android/app/src/main/java/com/noor/hotel_booking_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.noor.hotel_booking_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hotel_booking_app/view/screen/auth/sign_up.dart';
+import 'package:hotel_booking_app/view/screen/splash/splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignUp(),
+    );
+  }
+}
+
