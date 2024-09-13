84a16271c7f9d9ff97e7d40e5d75c11316c40efb
NS NooR <non377938@gmail.com>
2024-09-14 02:05:27 +0600

    discount

---
+++ lib/view/screen/hotel_details/widget/discount_show.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hotel_booking_app/utils/app_color.dart';
+import 'package:hotel_booking_app/view/common_widget/common_text.dart';
+
+class DiscountShow extends StatelessWidget {
+  const DiscountShow({
+    super.key, required this.text,
+  });
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: Get.height * 0.03,
+      width: Get.width * 0.15,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: const Color(0xffe6f3ff)),
+      child: Center(
+        child: CommonText(
+            text: text,
+            fColor: AppColor.primaryColor,
+            fSize: 10),
+      ),
+    );
+  }
+}
