19eacb8c6f6c26d0cda8eced1312084c574e37cd
NS NooR <non377938@gmail.com>
2024-09-14 02:05:57 +0600

    FilterButton

--- lib/view/screen/home/widget/filter_button.dart
+++ lib/view/screen/home/widget/filter_button.dart
@@ -12,17 +12,20 @@ class FilterButton extends StatelessWidget {
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.sizeOf(context).height;
     double screenWidth = MediaQuery.sizeOf(context).width;
-    return Container(
-      height: screenHeight * 0.07,
-      width: screenWidth * 0.17,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: AppColor.primaryColor),
-      child: const Center(
-        child: Icon(
-          Icons.filter_list_sharp,
-          color: Colors.white,
-          size: 40,
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: screenHeight * 0.07,
+        width: screenWidth * 0.17,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: AppColor.primaryColor),
+        child: const Center(
+          child: Icon(
+            Icons.filter_list_sharp,
+            color: Colors.white,
+            size: 40,
+          ),
         ),
       ),
     );
