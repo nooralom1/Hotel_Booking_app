38b2190073e889ece687e289dc94b2150d1905e2
NS NooR <non377938@gmail.com>
2024-09-14 02:06:06 +0600

    Home

--- lib/controller/getx_controller/home.dart
+++ lib/controller/getx_controller/home.dart
@@ -1,14 +1,53 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hotel_booking_app/controller/api_controller/business_accommodates.dart';
+import 'package:hotel_booking_app/controller/api_controller/recommended_hotels.dart';
+import 'package:hotel_booking_app/model/business_accommodates.dart';
+import 'package:hotel_booking_app/model/recommended_hotels.dart';
 
-class HomeController extends GetxController{
-
+class HomeController extends GetxController {
   RxBool isLoading = false.obs;
   TextEditingController searchController = TextEditingController();
+  RxList<RecommendedHotels> recommendedHotel = <RecommendedHotels>[].obs;
+  RxList<BusinessAccommodates> businessAccommodates =
+      <BusinessAccommodates>[].obs;
+  RangeValues currentRangeValues = const RangeValues(90, 350);
+  RxList<String> value = [
+    'Noor',
+    'Sakib',
+    'Fahim',
+    'Raju',
+    'Shovon',
+  ].obs;
+  var selectedValue = ''.obs;
+
+  getRecommendedHotels() async {
+    isLoading.value = true;
+    var data = await RecommendedHotelsService.recommendedHotelsService();
+    recommendedHotel.value = data?.recommendedHotels ?? [];
+    isLoading.value = false;
+  }
+
+  getBusinessAccommodates() async {
+    isLoading.value = true;
+    var data = await BusinessAccommodatesService.businessAccommodatesService();
+    businessAccommodates.value = data?.businessAccommodates ?? [];
+    isLoading.value = false;
+  }
 
   @override
   void dispose() {
     searchController.dispose();
     super.dispose();
+  }
+
+  @override
+  void onInit() {
+    getRecommendedHotels();
+    getBusinessAccommodates();
+    super.onInit();
   }
 }
