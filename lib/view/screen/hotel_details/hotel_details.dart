f82899989a7cdc947d71c03528ed9056650dd27b
NS NooR <non377938@gmail.com>
2024-09-15 03:19:26 +0600

    home

--- lib/view/screen/hotel_details/hotel_details.dart
+++ lib/view/screen/hotel_details/hotel_details.dart
@@ -1,31 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hotel_booking_app/model/recommended_hotels.dart';
 import 'package:hotel_booking_app/utils/app_color.dart';
 import 'package:hotel_booking_app/view/common_widget/common_back_button.dart';
 import 'package:hotel_booking_app/view/common_widget/common_button.dart';
 import 'package:hotel_booking_app/view/common_widget/common_text.dart';
+import 'package:hotel_booking_app/view/screen/booking_summary/booking_summary.dart';
 import 'package:hotel_booking_app/view/screen/hotel_details/widget/circleavatar_button.dart';
 import 'package:hotel_booking_app/view/screen/hotel_details/widget/discount_show.dart';
 import 'package:hotel_booking_app/view/screen/hotel_details/widget/rating_show.dart';
 
 class HotelDetails extends StatelessWidget {
-  const HotelDetails({super.key});
+  const HotelDetails({super.key, required this.id, this.recommendedHotels});
+  final int id;
+  final RecommendedHotels? recommendedHotels;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: CommonBackButton(),
-        title: CommonText(
+        leading: const CommonBackButton(),
+        title: const CommonText(
             text: "Hotel Details",
             fSize: 20,
             fWeight: FontWeight.bold,
             fColor: AppColor.primaryColor),
         centerTitle: true,
-        actions: [
+        actions: const [
           Icon(Icons.share, color: AppColor.primaryColor),
           Padding(
-            padding: const EdgeInsets.only(left: 10, right: 20),
+            padding: EdgeInsets.only(left: 10, right: 20),
             child: Icon(Icons.favorite_border, color: AppColor.primaryColor),
           )
         ],
@@ -33,10 +37,10 @@ class HotelDetails extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Container(
+            SizedBox(
               height: Get.height * 0.35,
               width: Get.width,
-              child: Image.asset("assets/database/Rectangle 77.png",
+              child: Image.network("${recommendedHotels?.image}",
                   fit: BoxFit.fill),
             ),
             Padding(
@@ -49,47 +53,43 @@ class HotelDetails extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       CommonText(
-                          text: "AYANA Resort",
+                          text: "${recommendedHotels?.name}",
                           fWeight: FontWeight.bold,
                           fSize: 16),
-                      CircleAvatarButton(icon: Icons.send)
+                      const CircleAvatarButton(icon: Icons.send)
                     ],
                   ),
                   SizedBox(height: Get.height * 0.01),
                   Row(
                     children: [
-                      DiscountShow(
-                        text: "10% OFF",
-                      ),
+                      DiscountShow(text: "${recommendedHotels?.discount}"),
                       SizedBox(width: Get.width * 0.04),
-                      RatingShow(
-                        text: "4.5 (120 Reviews)",
-                      ),
+                      RatingShow(text: "${recommendedHotels?.rating}"),
                     ],
                   ),
                   SizedBox(height: Get.height * 0.01),
                   Row(
                     children: [
-                      Icon(Icons.location_on_outlined,
+                      const Icon(Icons.location_on_outlined,
                           color: Colors.grey, size: 15),
                       SizedBox(width: Get.width * 0.02),
                       CommonText(
-                          text: "Karang Mas Estate, Jimbaran, Bali, Indonesia",
+                          text: "${recommendedHotels?.location}",
                           fColor: Colors.grey,
                           fSize: 10)
                     ],
                   ),
                   SizedBox(height: Get.height * 0.02),
-                  CommonText(
+                  const CommonText(
                       text: "Description", fSize: 16, fWeight: FontWeight.bold),
                   SizedBox(height: Get.height * 0.01),
-                  CommonText(
+                  const CommonText(
                       text:
                           "Nestled in the lush tropical paradise of Jimbaran, Bali, AYANA Resort and Spa offers an enchanting escape for travelers seeking luxury, relaxation, and breathtaking ocean views",
                       fSize: 10,
                       fColor: Colors.grey),
                   SizedBox(height: Get.height * 0.02),
-                  CommonText(
+                  const CommonText(
                       text: "Contact Info",
                       fWeight: FontWeight.bold,
                       fSize: 16),
@@ -102,7 +102,7 @@ class HotelDetails extends StatelessWidget {
                           CircleAvatarButton(
                               icon: Icons.person, color: Colors.blue.shade200),
                           SizedBox(width: Get.width * 0.04),
-                          Column(
+                          const Column(
                             children: [
                               CommonText(
                                   text: "John Mail", fWeight: FontWeight.bold),
@@ -116,19 +116,16 @@ class HotelDetails extends StatelessWidget {
                       ),
                       Row(
                         children: [
-                          CircleAvatarButton(icon: Icons.call),
+                          const CircleAvatarButton(icon: Icons.call),
                           SizedBox(width: Get.width * 0.03),
-                          CircleAvatarButton(icon: Icons.mail),
+                          const CircleAvatarButton(icon: Icons.mail),
                         ],
                       ),
                     ],
                   ),
                   SizedBox(height: Get.height * 0.03),
-                  CommonText(
-                    text: "Gallery",
-                    fSize: 16,
-                    fWeight: FontWeight.bold,
-                  ),
+                  const CommonText(
+                      text: "Gallery", fSize: 16, fWeight: FontWeight.bold),
                   SizedBox(height: Get.height * 0.03),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -136,18 +133,27 @@ class HotelDetails extends StatelessWidget {
                       Column(
                         children: [
                           CommonText(
-                              text: "\$350 USD",
+                              text: "${recommendedHotels?.price}",
                               fWeight: FontWeight.bold,
                               fSize: 16,
                               fColor: AppColor.primaryColor),
-                          CommonText(text: " /night"),
+                          const CommonText(text: " /night"),
                         ],
                       ),
                       CommonButton(
-                          height: Get.height * 0.06,
-                          width: Get.width * 0.53,
-                          btnName: "BOOK NOW",
-                          onTap: () {})
+                        height: Get.height * 0.06,
+                        width: Get.width * 0.53,
+                        btnName: "BOOK NOW",
+                        onTap: () {
+                          Get.to(
+                            () => BookingSummary(
+                              image: '${recommendedHotels?.image}',
+                              name: '${recommendedHotels?.name}',
+                              location: '${recommendedHotels?.location}',
+                            ),
+                          );
+                        },
+                      )
                     ],
                   )
                 ],
