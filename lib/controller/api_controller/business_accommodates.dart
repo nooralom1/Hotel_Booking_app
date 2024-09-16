577ae180d69633156a108484537a9dd5838280be
NS NooR <non377938@gmail.com>
2024-09-16 23:29:29 +0600

    hotel booking

--- lib/controller/api_controller/business_accommodates.dart
+++ lib/controller/api_controller/business_accommodates.dart
@@ -4,10 +4,12 @@ import 'package:hotel_booking_app/database/business_accommodates.dart';
 import 'package:hotel_booking_app/model/business_accommodates.dart';
 
 class BusinessAccommodatesService {
-  static Future<BusinessAccommodatesListModel?> businessAccommodatesService() async {
+  static Future<BusinessAccommodatesListModel?>
+      businessAccommodatesService() async {
     try {
-      BusinessAccommodatesListModel response = BusinessAccommodatesListModel.fromJson(
-          jsonDecode(jsonEncode(BusinessAccommodatesList.businessAccommodatesList)));
+      BusinessAccommodatesListModel response =
+          BusinessAccommodatesListModel.fromJson(jsonDecode(
+              jsonEncode(BusinessAccommodatesList.businessAccommodatesList)));
       return response;
     } catch (e) {
       debugPrint("Error: $e");
