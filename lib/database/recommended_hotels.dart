220fd8f2ec284d551161fdabd0e6f303827a624a
NS NooR <non377938@gmail.com>
2024-09-16 23:29:53 +0600

    hotel booking

--- lib/database/recommended_hotels.dart
+++ lib/database/recommended_hotels.dart
@@ -1,8 +1,9 @@
-class RecommendedHotelsList{
-  static Map<String,dynamic> recommendedHotelsList={
-    "recommendedHotels" : [
+class RecommendedHotelsList {
+  static Map<String, dynamic> recommendedHotelsList = {
+    "recommendedHotels": [
       {
-        "image": "https://github.com/user-attachments/assets/779d6261-b62a-492d-9399-42a889498db9",
+        "image":
+            "https://github.com/user-attachments/assets/779d6261-b62a-492d-9399-42a889498db9",
         "name": "AYANA Resort",
         "id": 01,
         "price": "\$200 - \$500 USD",
@@ -11,7 +12,8 @@ class RecommendedHotelsList{
         "location": "Bali, Indonesia"
       },
       {
-        "image": "https://github.com/user-attachments/assets/a9978b8a-8617-4995-95b9-67015867ab1b",
+        "image":
+            "https://github.com/user-attachments/assets/a9978b8a-8617-4995-95b9-67015867ab1b",
         "name": "SAFA Resort",
         "id": 02,
         "price": "\$300 - \$700 USD",
@@ -20,7 +22,8 @@ class RecommendedHotelsList{
         "location": "Bali, Indonesia"
       },
       {
-        "image": "https://github.com/user-attachments/assets/15277cfa-7f1a-4225-a964-7ac67a3bcc9d",
+        "image":
+            "https://github.com/user-attachments/assets/15277cfa-7f1a-4225-a964-7ac67a3bcc9d",
         "name": "RAJUK Resort",
         "id": 03,
         "price": "\$250 - \$650 USD",
@@ -29,7 +32,8 @@ class RecommendedHotelsList{
         "location": "Bali, Indonesia"
       },
       {
-        "image": "https://github.com/user-attachments/assets/4d319b65-cea2-4faf-9ebb-0274c41cba56",
+        "image":
+            "https://github.com/user-attachments/assets/4d319b65-cea2-4faf-9ebb-0274c41cba56",
         "name": "USA Resort",
         "id": 04,
         "price": "\$100 - \$300 USD",
@@ -38,7 +42,8 @@ class RecommendedHotelsList{
         "location": "Bali, Indonesia"
       },
       {
-        "image": "https://github.com/user-attachments/assets/1dc215b2-1bf3-42cf-a154-21e428027a25",
+        "image":
+            "https://github.com/user-attachments/assets/1dc215b2-1bf3-42cf-a154-21e428027a25",
         "name": "WTF Resort",
         "id": 05,
         "price": "\$400 - \$900 USD",
