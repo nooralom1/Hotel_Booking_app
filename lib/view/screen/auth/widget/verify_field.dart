9f7aae3b184660089b16967f18fd3b445f123b0f
NS NooR <non377938@gmail.com>
2024-09-16 23:32:18 +0600

    hotel booking

--- lib/view/screen/auth/widget/verify_field.dart
+++ lib/view/screen/auth/widget/verify_field.dart
@@ -3,8 +3,8 @@ import 'package:hotel_booking_app/utils/app_color.dart';
 
 class VerifyField extends StatelessWidget {
   const VerifyField({
-    super.key, required this.verifyController,
-
+    super.key,
+    required this.verifyController,
   });
   final TextEditingController verifyController;
 
@@ -14,16 +14,17 @@ class VerifyField extends StatelessWidget {
     double screenWidth = MediaQuery.sizeOf(context).width;
 
     return SizedBox(
-      height: screenHeight*0.07,
-      width: screenWidth*0.15,
+      height: screenHeight * 0.07,
+      width: screenWidth * 0.15,
       child: TextField(
         controller: verifyController,
         decoration: InputDecoration(
-          enabledBorder:
-          OutlineInputBorder(borderRadius: BorderRadius.circular(10),borderSide:  const BorderSide(color: Colors.grey)),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.grey)),
           focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(10),
-              borderSide:  const BorderSide(color: AppColor.primaryColor)),
+              borderSide: const BorderSide(color: AppColor.primaryColor)),
         ),
       ),
     );
