7dd604f6cefca9ec8dcff56ed056bf389899cf9c
NS NooR <non377938@gmail.com>
2024-09-18 01:47:51 +0600

    Chat

--- lib/view/common_widget/common_back_button.dart
+++ lib/view/common_widget/common_back_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hotel_booking_app/utils/app_color.dart';
 
 class CommonBackButton extends StatelessWidget {
   const CommonBackButton({
@@ -14,7 +13,7 @@ class CommonBackButton extends StatelessWidget {
         Get.back();
       },
       icon:
-          const Icon(Icons.arrow_back, color: AppColor.primaryColor, size: 25),
+          const Icon(Icons.arrow_back, color: Colors.white, size: 25),
     );
   }
 }
