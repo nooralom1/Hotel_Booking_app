c6721313f2c64fc10011c448647b0ad4ba9823b9
NS NooR <non377938@gmail.com>
2024-09-18 01:47:59 +0600

    Favorite

--- lib/view/screen/favorite/favorite.dart
+++ lib/view/screen/favorite/favorite.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hotel_booking_app/utils/app_color.dart';
-import 'package:hotel_booking_app/view/common_widget/common_back_button.dart';
 import 'package:hotel_booking_app/view/common_widget/common_text.dart';
 import 'package:hotel_booking_app/view/screen/favorite/widget/favorite_list_view.dart';
 
@@ -11,18 +10,18 @@ class Favorite extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          leading: const CommonBackButton(),
+          backgroundColor: AppColor.primaryColor,
           title: const CommonText(
               text: "Favorites",
               fSize: 20,
               fWeight: FontWeight.bold,
-              fColor: AppColor.primaryColor),
+              fColor: Colors.white),
           centerTitle: true,
         ),
         body: ListView.builder(
             itemCount: 5,
             itemBuilder: (context, index) {
-              return FavoriteListViewCard(
+              return const FavoriteListViewCard(
                   image: "assets/database/Rectangle 77.png",
                   name: "NOOR RESORT",
                   location: "Uttara, Dhaka",
