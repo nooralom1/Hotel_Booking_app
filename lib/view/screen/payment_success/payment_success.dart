b3f775208d9b09ebe5f70e2db22d57bf29e4c02e
NS NooR <non377938@gmail.com>
2024-09-17 23:36:54 +0600

    payment

--- lib/view/screen/payment_success/payment_success.dart
+++ lib/view/screen/payment_success/payment_success.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:hotel_booking_app/utils/app_color.dart';
 import 'package:hotel_booking_app/view/common_widget/common_button.dart';
 import 'package:hotel_booking_app/view/common_widget/common_text.dart';
-import 'package:hotel_booking_app/view/screen/home/home.dart';
+import 'package:hotel_booking_app/view/screen/bottom__nav_bar.dart';
 
 class PaymentSuccess extends StatelessWidget {
   const PaymentSuccess({super.key});
@@ -14,9 +14,9 @@ class PaymentSuccess extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            SizedBox(height:Get.height*0.04),
+            SizedBox(height: Get.height * 0.04),
             Image.asset("assets/image/payment_success.png"),
-            SizedBox(height:Get.height*0.04),
+            SizedBox(height: Get.height * 0.04),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 40),
               child: Column(
@@ -27,7 +27,7 @@ class PaymentSuccess extends StatelessWidget {
                     fWeight: FontWeight.bold,
                     fColor: AppColor.primaryColor,
                   ),
-                  SizedBox(height:Get.height*0.01),
+                  SizedBox(height: Get.height * 0.01),
                   const CommonText(
                     text: "Your hotel stay is secured.",
                     fSize: 20,
@@ -43,15 +43,15 @@ class PaymentSuccess extends StatelessWidget {
                     fSize: 20,
                     fColor: Colors.grey,
                   ),
-                  SizedBox(height:Get.height*0.04),
+                  SizedBox(height: Get.height * 0.04),
                   CommonButton(
                       height: Get.height * 0.06,
                       width: Get.width,
                       btnName: "GO HOME",
                       onTap: () {
-                        Get.to(() => const Home());
+                        Get.to(() => const BottomNavBar());
                       }),
-                  SizedBox(height:Get.height*0.03),
+                  SizedBox(height: Get.height * 0.03),
                   const CommonText(
                       text: "VIEW E-RECIEPT",
                       fSize: 15,
