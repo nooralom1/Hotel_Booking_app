11cdfff80586fbd4663fe532da8c98ea08ed31cb
NS NooR <non377938@gmail.com>
2024-09-17 22:34:37 +0600

    hotel booking

--- lib/view/screen/welcome/widget/custom_add_remove.dart
+++ lib/view/screen/welcome/widget/custom_add_remove.dart
@@ -15,17 +15,21 @@ class CustomAddRemove extends StatelessWidget {
       height: screenHeight * 0.07,
       width: screenWidth * 0.433,
       decoration: BoxDecoration(
-          borderRadius:
-          BorderRadius.circular(10),
-          border:
-          Border.all(color: Colors.grey)),
-      child: const Center(child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Icon(Icons.remove),
-          CommonText(text: "1",fSize: 20,),
-          Icon(Icons.add)
-        ],),),
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.grey)),
+      child: const Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Icon(Icons.remove),
+            CommonText(
+              text: "1",
+              fSize: 20,
+            ),
+            Icon(Icons.add)
+          ],
+        ),
+      ),
     );
   }
 }
