30dbf4b819ca2be129b558157e3504c880ed4cfc
NS NooR <non377938@gmail.com>
2024-09-19 01:48:46 +0600

    Booking

--- lib/view/screen/booking_list/booking_list.dart
+++ lib/view/screen/booking_list/booking_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hotel_booking_app/controller/getx_controller/booking_list.dart';
 import 'package:hotel_booking_app/utils/app_color.dart';
 import 'package:hotel_booking_app/view/common_widget/common_text.dart';
 import 'package:hotel_booking_app/view/screen/booking_list/widget/booking_list_view.dart';
@@ -8,30 +10,41 @@ class BookingList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    BookingListController controller = Get.put(BookingListController());
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColor.primaryColor,
-        title: const CommonText(
-            text: "Booking List",
-            fSize: 20,
-            fWeight: FontWeight.bold,
-            fColor: Colors.white),
-        centerTitle: true,
-      ),
-      body: ListView.builder(
-        itemCount: 2,
-        itemBuilder: (context, index) {
-          return BookingListViewCard(
-            image: "assets/database/Rectangle 77.png",
-            name: "NOOR RESORT",
-            checkIn: "Check-in: 24-11-24",
-            checkOut: "Check-out: 26-11-24",
-            guest: "Guest: 3",
-            room: "Room: 1",
-            onTap: () {},
-          );
-        },
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: AppColor.primaryColor,
+          title: const CommonText(
+              text: "Booking List",
+              fSize: 20,
+              fWeight: FontWeight.bold,
+              fColor: Colors.white),
+          centerTitle: true,
+        ),
+        body: Obx(() => controller.isLoading.isFalse
+            ? Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: ListView.builder(
+                  itemCount: controller.bookingList.length,
+                  itemBuilder: (context, index) {
+                    return BookingListViewCard(
+                      image: "${controller.bookingList[index].image}",
+                      name: "${controller.bookingList[index].name}",
+                      checkIn:
+                          "Check-in: ${controller.bookingList[index].checkIn}",
+                      checkOut:
+                          "Check-out: ${controller.bookingList[index].checkOut}",
+                      guest: "Guest: ${controller.bookingList[index].guest}",
+                      room: "Room: ${controller.bookingList[index].room}",
+                      onTap: () {
+                        Get.snackbar("message", "Canceled");
+                      },
+                    );
+                  },
+                ),
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              )));
   }
 }
