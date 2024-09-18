7291e004a209cf7dd8b0e5320e59f4131c5fadfb
NS NooR <non377938@gmail.com>
2024-09-18 11:58:45 +0600

    booking

--- lib/view/screen/booking_summary/booking_summary.dart
+++ lib/view/screen/booking_summary/booking_summary.dart
@@ -7,12 +7,12 @@ import 'package:hotel_booking_app/view/common_widget/common_text.dart';
 import 'package:hotel_booking_app/view/screen/Payment/payment.dart';
 
 class BookingSummary extends StatelessWidget {
-  const BookingSummary(
-      {super.key,
-      required this.image,
-      required this.name,
-      required this.location,
-      });
+  const BookingSummary({
+    super.key,
+    required this.image,
+    required this.name,
+    required this.location,
+  });
   final String image;
   final String name;
   final String location;
@@ -45,17 +45,14 @@ class BookingSummary extends StatelessWidget {
                         width: Get.width * 0.35,
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
-                            image:
-                                DecorationImage(image: NetworkImage(image))),
+                            image: DecorationImage(image: NetworkImage(image))),
                       ),
                       SizedBox(width: Get.width * 0.04),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           CommonText(
-                              text: name,
-                              fSize: 16,
-                              fWeight: FontWeight.bold),
+                              text: name, fSize: 16, fWeight: FontWeight.bold),
                           SizedBox(height: Get.height * 0.01),
                           CommonText(
                             text: location,
@@ -87,8 +84,10 @@ class BookingSummary extends StatelessWidget {
                       const CommonText(
                           text: "Booking Date", fWeight: FontWeight.bold),
                       CommonText(
-                          text:
-                              DateTime.now().toString().substring(0, 10).toString(),
+                          text: DateTime.now()
+                              .toString()
+                              .substring(0, 10)
+                              .toString(),
                           fSize: 16,
                           fColor: Colors.grey)
                     ],
@@ -178,7 +177,9 @@ class BookingSummary extends StatelessWidget {
                       height: Get.height * 0.06,
                       width: Get.width,
                       btnName: "CONTINUE TO PAYMENT",
-                      onTap: () {Get.off(()=>const Payment());})
+                      onTap: () {
+                        Get.off(() => const Payment());
+                      })
                 ],
               ),
             ),
