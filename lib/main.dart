fa79e79f0e1a3303162ad92b84a6ff485d83ebbf
NS NooR <non377938@gmail.com>
2024-09-20 01:51:56 +0600

    modify

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
     );
   }
